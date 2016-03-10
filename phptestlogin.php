cd93e3e36a474a240796033d79784779703cf667
John Moosemiller <itcaughtonj@gmail.com>
2016-03-09 18:04:27 -0600

    Create phptestlogin.php

---
+++ phptestlogin.php
@@ -0,0 +1,20 @@
+<?php
+$fields = array(
+	'username' => urlencode($_POST['last_name']),
+	'password' => urlencode($_POST['first_name']),
+);
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+  $curl_handle=curl_init();
+  curl_setopt($curl_handle,CURLOPT_URL,'https://api.robinhood.com/quotes/api-token-auth/')
+  curl_setopt($curl_handle,CURLOPT_POST,2);
+  curl_setopt($curl_handle,CURLOPT_POSTFIELDS, $fields_string);
+  $buffer = curl_exec($curl_handle);
+  curl_close($curl_handle);
+  if (empty($buffer)){
+      print "Nothing returned from url.<p>";
+  }
+  else{
+      print $buffer;
+  }
+?>
