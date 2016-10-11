9a370efc393b6659f5225126da71bc0132c86a6b
John Moosemiller <itcaughtonj@gmail.com>
2016-10-10 22:25:46 -0500

    Update Tickers.m

--- Tickers.m
+++ Tickers.m
@@ -1,12 +1,11 @@
 %tickers1 = {'AAPL','AXP','BA','CAT','CSCO','CVX','KO','DD','XOM','GE','GS','HD','IBM','INTC','JNJ','JPM','MCD','MMM','MRK','MSFT','NKE','PFE','PG','TRV','UNH','UTX','V','VZ','WMT','DIS'}
 [num,txt,raw]=xlsread('companylist.csv');
-tickervals = txt(2:500,1);
+tickervals = txt(2:500,1);%up to 3145
 
 FID=fopen('test.txt','w');
 for i = 1:length(tickervals)
     full = strcat('https://api.robinhood.com/quotes/historicals/',tickervals(i),'/');
     full2=char(full);
-    i
     a = {urlread(full2,'Get',{'interval','month'})};
     fprintf(FID,'%s\r\n',a{:});
 end
